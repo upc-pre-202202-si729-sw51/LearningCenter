258a88880051d2473a4da686e425b427cc1295c5
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-12 18:43:37 -0500

    feat(students): added controller actions.

--- src/main/java/com/acme/learningcenter/learning/api/StudentsController.java
+++ src/main/java/com/acme/learningcenter/learning/api/StudentsController.java
@@ -2,10 +2,15 @@ package com.acme.learningcenter.learning.api;
 
 import com.acme.learningcenter.learning.domain.model.entity.Student;
 import com.acme.learningcenter.learning.domain.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.acme.learningcenter.learning.mapping.StudentMapper;
+import com.acme.learningcenter.learning.resource.CreateStudentResource;
+import com.acme.learningcenter.learning.resource.StudentResource;
+import com.acme.learningcenter.learning.resource.UpdateStudentResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,18 +19,36 @@ import java.util.List;
 public class StudentsController {
     private final StudentService studentService;
 
-    public StudentsController(StudentService studentService) {
+    private final StudentMapper mapper;
+
+    public StudentsController(StudentService studentService, StudentMapper mapper) {
         this.studentService = studentService;
+        this.mapper = mapper;
     }
 
     @GetMapping
-    public List<Student> getAllStudents() {
-        return studentService.getAll();
+    public Page<StudentResource> getAllStudents(Pageable pageable) {
+        return mapper.modelListPage(studentService.getAll(), pageable);
     }
 
     @GetMapping("{studentId}")
-    public Student getStudentById(@PathVariable Long studentId) {
-        return studentService.getById(studentId);
+    public StudentResource getStudentById(@PathVariable Long studentId) {
+        return mapper.toResource(studentService.getById(studentId));
+    }
+
+    @PostMapping
+    public ResponseEntity<StudentResource> createStudent(@RequestBody CreateStudentResource resource) {
+        return new ResponseEntity<>(mapper.toResource(studentService.create(mapper.toModel(resource))), HttpStatus.CREATED);
+    }
+    @PutMapping("{studentId}")
+    public StudentResource updateStudent(@PathVariable Long studentId,
+                                         @RequestBody UpdateStudentResource resource) {
+        return mapper.toResource(studentService.update(studentId, mapper.toModel(resource)));
+    }
+
+    @DeleteMapping("{studentId}")
+    public ResponseEntity<?> deleteStudent(@PathVariable Long studentId) {
+        return studentService.delete(studentId);
     }
 
 }
