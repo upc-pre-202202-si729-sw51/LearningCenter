ae94b3d5fdfb26c712cd6268a09ac0f151b331c2
Jorge Mayta <jmayta@pucp.edu.pe>
2022-10-12 18:10:15 -0500

    feat(students): added student resource mapping configuration.

---
+++ src/main/java/com/acme/learningcenter/learning/mapping/StudentMapper.java
@@ -0,0 +1,37 @@
+package com.acme.learningcenter.learning.mapping;
+
+import com.acme.learningcenter.learning.domain.model.entity.Student;
+import com.acme.learningcenter.learning.resource.CreateStudentResource;
+import com.acme.learningcenter.learning.resource.StudentResource;
+import com.acme.learningcenter.learning.resource.UpdateStudentResource;
+import com.acme.learningcenter.shared.mapping.EnhancedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class StudentMapper implements Serializable {
+
+    @Autowired
+    EnhancedModelMapper mapper;
+
+    public StudentResource toResource(Student model) {
+        return mapper.map(model, StudentResource.class);
+    }
+
+    public Student toModel(CreateStudentResource resource) {
+        return mapper.map(resource, Student.class);
+    }
+
+    public Student toModel(UpdateStudentResource resource) {
+        return mapper.map(resource, Student.class);
+    }
+
+    public Page<StudentResource> modelListPage(List<Student> modelList, Pageable pageable) {
+        return new PageImpl<>(mapper.mapList(modelList, StudentResource.class), pageable, modelList.size());
+    }
+
+}
