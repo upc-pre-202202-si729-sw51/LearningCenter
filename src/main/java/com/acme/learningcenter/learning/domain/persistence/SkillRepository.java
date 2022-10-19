2e5da67fe39e3b8d51a6d28d2ed0920631a36f45
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-19 17:28:05 -0500

    feat(skills): added persistence layer repositories.

--- src/main/java/com/acme/learningcenter/learning/domain/persistence/SkillRepository.java
+++ src/main/java/com/acme/learningcenter/learning/domain/persistence/SkillRepository.java
@@ -4,7 +4,9 @@ import com.acme.learningcenter.learning.domain.model.entity.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Long> {
-
+    Optional<Skill> findByName(String name);
 }
