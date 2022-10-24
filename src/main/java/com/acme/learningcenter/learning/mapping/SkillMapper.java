590d2ef60a0a8f991a0a3d6afb41b6a20ac19af3
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-24 16:36:36 -0500

    feat(skills): implemented basic mapper.

---
+++ src/main/java/com/acme/learningcenter/learning/mapping/SkillMapper.java
@@ -0,0 +1,15 @@
+package com.acme.learningcenter.learning.mapping;
+
+import com.acme.learningcenter.shared.mapping.EnhancedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+
+public class SkillMapper implements Serializable {
+
+    @Autowired
+    EnhancedModelMapper mapper;
+
+
+
+}
