7fc5961d3a8409948c295d29430b1633fab077de
Angel Velasquez <pcisavel@upc.edu.pe>
2022-11-07 17:47:29 -0500

    feature(security): added UserDetails implementation class.

---
+++ src/main/java/com/acme/learningcenter/security/middleware/JwtHandler.java
@@ -0,0 +1,26 @@
+package com.acme.learningcenter.security.middleware;
+
+
+import org.apache.logging.log4j.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtHandler.class);
+
+    @Value("${authorization.jwt.secret}")
+    private String secret;
+
+    @Value("${authorization.jwt.expiration.days}")
+    private int expirationDays;
+
+    public String generateToken(Authentication authentication) {
+        // TODO: implement
+        return Strings.EMPTY;
+    }
+}
